1b9ea0e83e2d3959a6c302b53ef1619295e48b9c
qixin <qixin@haoqix.com>
2018-10-12 11:15:30 +0800

    project create

---
+++ README.md
@@ -0,0 +1,11 @@
+######
+	webpack exercise
+
+	参考
+		webpack 中文网：https://www.webpackjs.com/concepts/
+		webpack 网：https://webpack.js.org/concepts/
+
+	###branch && tag
+		t0.0.1 概念
+			src/webpack/concepts
+	
---
+++ src/webpackConfig/concepts.js
@@ -0,0 +1,32 @@
+/**
+ * create at 10/12/18
+ *  概念
+ */
+
+'use strict';
+
+const path = require('path')
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+	entry: './path/to/my/entry/file.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'my-first-webpack.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.txt$/,
+				use: 'raw-loader',
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({template: './src/index.html'})
+	],
+	mode: 'development' // production development
+}
+
+module.exports = config;
